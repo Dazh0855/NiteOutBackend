8f989d2686fce8f04616e70f7835e660973d9dca
Michael Gilroy <michael.gilroy24@gmail.com>
2017-12-01 01:10:05 +0000

    new urls, new views

--- appbackend/api/urls.py
+++ appbackend/api/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.index, name="index")
+	url(r'^$', views.index, name="index"),
+	url(r'^getEvent$', views.getEvent, name="getEvent"),
+	url(r'^getUser$', views.getUser, name="getUser"),
 ]
