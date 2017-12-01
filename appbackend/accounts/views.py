6b18fa9e31e5fd52f2109065cfdbae6f12ecc7fa
Dazh0855 <Dazh0855@colorado.edu>
2017-12-01 02:08:51 +0000

    added login url

--- appbackend/accounts/views.py
+++ appbackend/accounts/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
 	return HttpResponse("post here")	
+
+def login(request):
+	if method.request == "POST":
+		x = request.POST.get('user_name','password')
+		return x
+
