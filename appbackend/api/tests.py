e91413f6a6b3c4c5b93b5b321f65438f7a9bc02e
Dazh0855 <Dazh0855@colorado.edu>
2017-12-03 23:53:49 +0000

    Tests for Users and Events database, are not correct just yet

--- appbackend/api/tests.py
+++ appbackend/api/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
-
+from django.db import models
 # Create your tests here.
+
+class TestModels(TestCase):
+	def SetUp(self):
+		Users.objects.create(user_name="name", user_info="info", user_password="password", user_id=2)
+		Events.objects.create(title="title",price = "price")
+	def test_database(self):
+		name = Users.objects.get(user_name="name")
+		password = Users.objects.get(user_password="password")
+		info = Users.objects.get(user_info="info")
+		title = Events.objects.get(title="title")
+		price = Events.objects.get(price="price")
+		self.assertEqual(name,"name")
+		self.assertEqual(password,"password")
+		self.assertEqual(info,"info")
+		self.assertEqual(user_id,2)
+	
+		
