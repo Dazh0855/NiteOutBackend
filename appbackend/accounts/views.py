15fac264574f040f3475d941b1a10bd9491578a0
Dazh0855 <Dazh0855@colorado.edu>
2017-12-12 01:55:29 +0000

    added to views

--- appbackend/accounts/views.py
+++ appbackend/accounts/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db import models
+from django import forms
+from api.forms import UsersForm
+from api.models import Users
 from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
@@ -11,5 +15,11 @@ def login(request):
 	if request.method == "POST":
 		email = request.POST.get('email','')
 		passwd = request.POST.get('password','')
+		x = Users.objects.filter(user_name=email,user_password=passwd).exists()
+		if x == True:
+			x = Users.objects.get(user_name = email)
+			return HttpResponse(x.user_name)
+		else:
+			user = Users.objects.create(user_name = email, user_password=passwd,)
 		return HttpResponse(email)
 
